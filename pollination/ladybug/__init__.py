f68eb7f4192f1fb23b478adc3573bea628b1f916
Chris Mackey <chris@ladybug.tools>
2021-02-22 23:28:47 -0500

    fix(docker): Reconcile difference between PyPI and docker image name

--- pollination/ladybug/__init__.py
+++ pollination/ladybug/__init__.py
@@ -4,7 +4,7 @@ from pollination_dsl.common import get_docker_image_from_dependency
 # set the version for docker image dynamically based on ladybug version
 # in dependencies
 image_id = get_docker_image_from_dependency(
-    __package__, 'ladybug', 'ladybugtools'
+    __package__, 'ladybug-core', 'ladybugtools', 'ladybug'
 )
 
 __pollination__ = {
